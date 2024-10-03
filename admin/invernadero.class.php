b7041cfe996cdbd9b76b961e6353e91854988bc6
Christian Muñoz <lalodm69@gmail.com>
2024-10-03 11:34:19 -0600

    commit2

--- admin/invernadero.class.php
+++ admin/invernadero.class.php
@@ -24,9 +24,22 @@ class Invernadero extends Sistema{
     }
     
     function update($id, $data) {
+        $this->conexion();
         $result=[];
+        $sql = "UPDATE invernadero SET invernadero=:invernadero, latitud=:latitud, longitud=:longitud,
+        area=:area, fecha_creacion=:fecha_creacion WHERE id_invernadero=:id_invernadero;";
+        $modificar = $this->con->prepare($sql);
+        $modificar -> bindParam(':id_invernadero', $id,PDO::PARAM_INT);
+        $modificar -> bindParam(':invernadero', $data['invernadero'],PDO::PARAM_STR);
+        $modificar -> bindParam(':latitud', $data['latitud'],PDO::PARAM_STR);
+        $modificar -> bindParam(':longitud', $data['longitud'],PDO::PARAM_STR);
+        $modificar -> bindParam(':area', $data['area'],PDO::PARAM_INT);
+        $modificar -> bindParam(':fecha_creacion', $data['fecha_creacion'],PDO::PARAM_STR);
+        $modificar -> execute();
+        $result =$modificar->rowCount();
         return $result;
     }
+
     function delete($id) {
         $result=[];
         $this -> conexion();
@@ -38,7 +51,13 @@ class Invernadero extends Sistema{
         return $result;
     }
     function readOne($id) {
+        $this -> conexion();
         $result=[];
+        $query = "SELECT * FROM invernadero where id_invernadero = :id_invernadero;";
+        $sql = $this -> con ->prepare($query);
+        $sql -> bindParam(":id_invernadero", $id, PDO::PARAM_INT);
+        $sql->execute();
+        $result = $sql -> fetch(PDO::FETCH_ASSOC);
         return $result;
     }
     function readAll(){
