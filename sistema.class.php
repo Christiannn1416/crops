6efb845e2b86b0b102236055dd95f9b0bd2c123a
Christian Muñoz <lalodm69@gmail.com>
2024-10-10 21:17:25 -0600

    se empezó el sistema de usuarios

--- sistema.class.php
+++ sistema.class.php
@@ -1,13 +1,52 @@
 <?php
-include ('config.class.php');
-    class Sistema{
-        var $con;
-        function conexion(){
-            $this -> con = new PDO(SGBD.':host='.DBHOST.';dbname='.DBNAME.';port='.DBPORT, DBUSER, DBPASS);
-        }
-
-        function alert($tipo,$mensaje){
-            include('views/alert.php');
-        }
+include('config.class.php');
+class Sistema
+{
+    var $con;
+    function conexion()
+    {
+        $this->con = new PDO(SGBD . ':host=' . DBHOST . ';dbname=' . DBNAME . ';port=' . DBPORT, DBUSER, DBPASS);
     }
+
+    function alert($tipo, $mensaje)
+    {
+        include('views/alert.php');
+    }
+    function getRol($correo)
+    {
+        $this->conexion();
+        $data = [];
+        if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            $sql = "select r.rol
+                from usuario u join usuario_rol ur on u.id_usuario = ur.id_usuario
+                join rol r on ur.id_rol = r.id_rol 
+                where u.correo=:correo;";
+            $roles = $this->con->prepare($sql);
+            $roles->bindParam(':correo', $correo, PDO::PARAM_STR);
+            $roles->execute();
+            $data = $roles->fetchAll(PDO::FETCH_ASSOC);
+        }
+        return $data;
+    }
+    function getPrivilegio($correo)
+    {
+        $this->conexion();
+        $data = [];
+        if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            $sql = "select p.permiso
+                    from usuario u join usuario_rol ur on u.id_usuario = ur.id_usuario
+                    join rol r on ur.id_rol = r.id_rol
+                    join rol_permiso rp on r.id_rol = rp.id_rol
+                    join permiso p on rp.id_permiso = p.id_permiso 
+                    where u.correo =:correo ;";
+            $privilegio = $this->con->prepare($sql);
+            $privilegio->bindParam(':correo', $correo, PDO::PARAM_STR);
+            $privilegio->execute();
+            $data = $privilegio->fetchAll(PDO::FETCH_ASSOC);
+        }
+        return $data;
+    }
+    
+
+}
 ?>
