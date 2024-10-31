def9f2ec675563e4af737a5f3988654d80293917
Christian Muñoz <lalodm69@gmail.com>
2024-10-30 22:45:09 -0600

    x

--- admin/views/header_admin.php
+++ admin/views/header_admin.php
@@ -21,6 +21,10 @@
                                 <li><a class="dropdown-item" href="seccion.php">Secciones</a></li>
                             </ul>
                         </li>
+                        <li class="nav-item">
+                            <a href="empleado.php">Empleado</a>
+
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
