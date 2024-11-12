6c3555162d19627db153275875032e374d33f40a
Christian Muñoz <lalodm69@gmail.com>
2024-11-11 19:04:32 -0600

    metemos librería de pdf

---
+++ vendor/spipu/html2pdf/examples/example00.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Html2Pdf Library - example
+ *
+ * HTML => PDF converter
+ * distributed under the OSL-3.0 License
+ *
+ * @package   Html2pdf
+ * @author    Laurent MINGUET <webmaster@html2pdf.fr>
+ * @copyright 2023 Laurent MINGUET
+ */
+require_once('../../../autoload.php');
+
+use Spipu\Html2Pdf\Html2Pdf;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+
+try {
+    ob_start();
+    $content = ob_get_clean();
+    $content = '
+    <html>
+    <body>
+        <h1> Esta es una pueba</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </body>
+    </html>
+    ';
+    $html2pdf = new Html2Pdf('P', 'A4', 'fr');
+    $html2pdf->setDefaultFont('Arial');
+    $html2pdf->writeHTML($content);
+    $html2pdf->output('ejemplo.pdf');
+} catch (Html2PdfException $e) {
+    $html2pdf->clean();
+
+    $formatter = new ExceptionFormatter($e);
+    echo $formatter->getHtmlMessage();
+}
